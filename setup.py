6311477478fc99cd5c1cf307fc7e495c688c7510
FreedomOfRestriction <FreedomOfRestriction@openmailbox.org>
2015-10-12 11:13:18 -0400

    PyMaster 0.12
    * Added a rule requiring the slave to check in with the master/mistress.
    * Added support for "routines".
    * Added variations to the Oath of Servitude.
    * Changed the restriction to using the toilet to a restriction on peeing.
    * Sick slaves are no longer punished for midnight requests.
    * Extension system now supports deleting things.
    * Recently abandoned chores are now not repeated.
    * Forget times have been reduced so you are hurt by not doing tasks faster.
    * Activities can now be randomly skipped, so they are less predictable.
    * The way the rules are presented has been tweaked.
    * New punishments, tasks, and gifts + tweaks to existing ones.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ executables = [
 ]
 
 setup(name = 'PyMaster',
-      version = '0.11',
+      version = '0.12',
       description = 'Virtual master/mistress program.',
       options = dict(build_exe = buildOptions),
       executables = executables)
